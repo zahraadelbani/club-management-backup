e197a5ab3ea9bbc5d1aceffaabb11b44e6f7027f
zahraadelbani <zahraadelbani4@gmail.com>
2025-05-21 19:31:14 +0300

    Overwrite: full working project with data

--- club_management/settings.py
+++ club_management/settings.py
@@ -263,12 +263,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Database Configuration
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
